45e5ee213b4badca882d70c7cfae331527b726f7
Nicki de Wet <github@dewet.org.za>
2022-07-02 21:13:51 +0200

    first commit

---
+++ LoadSheddingParser.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from html.parser import HTMLParser
+import urllib.request
+import re
+
+class LoadSheddingParser(HTMLParser):
+    isLoadsheddingMsg = False
+    isLoadsheddingP = False
+    fullMsg = ""
+    def handle_starttag(self, tag, attrs):
+        if (tag == 'div'):
+            for key,value in attrs:
+                if (key == 'class' and value=='alertBody'):
+                    self.isLoadsheddingMsg = True
+        if (self.isLoadsheddingMsg == True and tag == 'p'):
+            self.isLoadsheddingP = True
+
+    def handle_data(self, data):
+        if (self.isLoadsheddingP):
+            #print("Data     :", data)
+            self.fullMsg += data
+
+    def handle_endtag(self, tag):
+        if (self.isLoadsheddingP and tag == 'p'):
+            self.isLoadsheddingP = False
+        if (self.isLoadsheddingMsg and tag == 'div'):
+            self.isLoadsheddingMsg = False
+
+page = urllib.request.urlopen("https://www.capetown.gov.za/")
+content = page.read()
+parser = LoadSheddingParser()
+parser.feed(content.decode("utf-8"))
+
+message = re.findall("City customers on Stage (\d) from (\d{2}):00 - (\d{2}):00, then.Stage (\d) from (\d{2}):00 - (\d{2}):00 on (\w+)", parser.fullMsg)
+
+matches = message[0]
+
+stage = int(matches[0])
+stagefrom = int(matches[1])
+stageto=int(matches[2])
+
+if (len(matches) == 7):
+    stagenext=int(matches[3])
+    stagenextfrom=int(matches[4])
+    stagenextto=int(matches[5])
+    stagenextday=matches[6]
+
+time = datetime.now().time()
+hour = time.hour
+if (stagefrom < stageto):
+    if (hour > stagefrom and hour < stageto):
+        currentStage = stage
+
+if (stagenextfrom > stagenextto):
+    if (hour > stagenextfrom or hour < stagenextto):
+        currentStage = stagenext
+
+print("Stage ", currentStage)
