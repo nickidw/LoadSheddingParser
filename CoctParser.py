4e553aa03b14826f8cd4f436d475d30e32910287
Nicki de Wet <github@dewet.org.za>
2022-10-01 14:42:18 +0200

    Get in/next loadshedding

--- CoctParser.py
+++ CoctParser.py
@@ -1,12 +1,16 @@
 from datetime import datetime
 import yaml
 import urllib.request
+import pandas as pd
+import pytz
 
 def main():
-    print("hello")
     f = urllib.request.urlopen("https://raw.githubusercontent.com/beyarkay/eskom-calendar/main/manually_specified.yaml")
     content = f.read()
+    f.close
     yml = yaml.safe_load(content)
+    
+    now = datetime.now()
     
     for change in yml['changes']:
         coct = False
@@ -14,9 +18,23 @@ def main():
             if 'include' in k and 'coct' in v:
                 coct = True
         if coct:
-            now = datetime.now()
             if change['start'] < now and change['finsh'] > now:
                 print(change['stage'])
-    #print(yml)
+ 
+    utc = pytz.timezone('Africa/Johannesburg')
+    nowutc = utc.localize(now)
 
+    url = "https://github.com/beyarkay/eskom-calendar/releases/download/latest/machine_friendly.csv"
+    df = pd.read_csv(url, parse_dates=['start', 'finsh'])
+    coct1 = df[df['area_name'].str.endswith('city-of-cape-town-area-1')]
+    prevstart = nowutc
+    for i in range(coct1.index.size-1):
+        start = coct1.iloc[i]["start"]
+        finsh = coct1.iloc[i]["finsh"]
+
+        if nowutc > start and nowutc < finsh:
+            print(f"In loadshedding From {start} to {finsh}")
+        if prevstart < nowutc and start > nowutc:
+            print(f"Next loadshedding From {start} to {finsh}")
+        prevstart = start
 main()
